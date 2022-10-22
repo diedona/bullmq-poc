3324e1ecd250a3d6c318703db290579417b9e46a
Diego Doná <diedona@gmail.com>
2022-10-21 23:46:49 -0300

    setup inicial projeto

---
+++ src/web-app/server.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || "3001";
+
+app.get('/', (req: Request, resp: Response) => {
+    resp.json({ ok: true });
+});
+
+app.listen(port, () => {
+    console.log(`listening at port ${port}`);
+})
